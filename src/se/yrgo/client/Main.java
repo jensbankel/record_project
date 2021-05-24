f98cd1e953692032e6df3be49f28ff0dc9fa7d68
jensbankel <jenban1204@skola.goteborg.se>
2021-05-24 10:53:19 +0200

    added more records to main to populate the database

--- src/se/yrgo/client/Main.java
+++ src/se/yrgo/client/Main.java
@@ -28,15 +28,27 @@ public class Main {
                         + "RecordManagementServiceImplementation!se.yrgo.service."
                         + "RecordManagementService");
         
-        Record rec1 = new Record("Abba", "Collected", "Pop", "0600753337776");
-		Record rec2 = new Record("Charlie Parker", "Bird in LA", "Jazz", "0602507408459");
-		Record rec3 = new Record("Nirvana", "Nevermind", "Rock", "0720642442517");
-		
-		
-		//Regitrerar employeeer i databasen!
-		service.registerRecord(rec1);
-		service.registerRecord(rec2);
-		service.registerRecord(rec3);
+        Record record1 = new Record("Abba", "Collected", "Pop", "0600753337776");
+        Record record2 = new Record("Charlie Parker", "Bird in LA", "Jazz", "0602507408459");
+        Record record3 = new Record("Nirvana", "Nevermind", "Rock", "0720642442517");
+        Record record4 = new Record("Carola", "Blott en dag", "Pop", "0044001660023");
+        Record record5 = new Record("Arvingarna", "I do", "Pop", "0190759527627");
+        Record record6 = new Record("Madonna", "Like A Virgin", "Pop", "0603497849284");
+        Record record7 = new Record("Sun Ra", "Lanquidity", "Jazz", "4062548021080");
+        Record record8 = new Record("Dropkick Murphys", "Turn up the dial", "Punk", "5400863049126");
+        Record record9 = new Record("Nattskiftet", "Stämplar in", "Electronic", " 7393210769311");
+        
+        
+        //Regitrerar employeeer i databasen!
+        service.registerRecord(record1);
+        service.registerRecord(record2);
+        service.registerRecord(record3);
+        service.registerRecord(record4);
+        service.registerRecord(record5);
+        service.registerRecord(record6);
+        service.registerRecord(record7);
+        service.registerRecord(record8);
+        service.registerRecord(record9);
 
         List<Record> records = service.getAllRecords();
         for (Record r : records) {
